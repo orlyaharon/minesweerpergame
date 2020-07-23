91ecf2c30bc0946a45791e53c72935162971f22d
orlyaharon <68657096+orlyaharon@users.noreply.github.com>
2020-07-23 20:27:43 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
 'use strict'
 
 const MINE = '💣';
-
+const FLAG = '🚩';
 
 var gBoard;
 var bordSize;
-var gIsShown = true;
+var gTime = 0;
+var setTimeInterval;
+
+var gLevel = {
+    SIZE: 4,
+    MINES: 2
+};
+
+var gGame = {
+    isOn: false,
+    shownCount: 0,
+    markedCount: 0,
+    secsPassed: 0
+}
+
 function initGame() {
     gBoard = buildBoard();
     renderBoard(gBoard);
-    setMinesNegsCount(gBoard);
+    // setTime();
 }
 
 function buildBoard(height = 4, width = 4) {
@@ -27,66 +41,109 @@ function buildBoard(height = 4, width = 4) {
             board[i][j] = cell;
         }
     }
-
-    // יש לשנות לאחר מכן את הנחת המוקשים באופן רנדומלי
-
+    board[getRandomIntInclusive(0, 3)][getRandomIntInclusive(0, 3)].isMine = true;
+    board[getRandomIntInclusive(0, 3)][getRandomIntInclusive(0, 3)].isMine = true;
     console.table(board);
     return board;
 }
 
 
 function renderBoard(board) {
+    // debugger
     var strHTML = '';
     for (var i = 0; i < board.length; i++) {
-        strHTML += '<tr>\n';
+        strHTML += '<tr>';
         for (var j = 0; j < board[0].length; j++) {
+            var pos = { i: i, j: j }
+            var neighbours = setMinesNegsCount(pos);
             var cell = board[i][j];
-            // console.log(cell);
-            var className = 'cell ' + i + '-' + j;
-            console.log(className);
-
-            strHTML += `\t<td class="cell ${className}"  onclick="cellClicked(elCell, i, j)"  >\n`;
-            strHTML += `\t${cell}</td>\n`;
+            cell.minesAroundCount = neighbours;
+            var className = i + '-' + j;
+            
+            strHTML += `<td class="cell ${className}"  onclick="cellClicked(${cell}, ${pos})"  >`;
+            if (cell.isMine) {
+                strHTML += `💣</td>`
+            }
+            if (cell.minesAroundCount) {
+                strHTML += `${neighbours}</td>`;
+            } else {
+                continue;
+            }
+            console.log(cell);
         }
-        strHTML += '</tr>\n';
+        strHTML += '</tr>';
     }
-
-    // console.log('strHTML is:');
-    // console.log(strHTML);
-
-
     var elBoard = document.querySelector('.board');
     elBoard.innerHTML = strHTML;
     return board;
 }
 
 
-function setMinesNegsCount(board) {
-    console.log(board);
+function setMinesNegsCount(pos) {
     var count = 0;
-    for (var i = board[i] - 1; i <= board[i] + 1; i++) {
+    for (var i = pos.i - 1; i <= pos.i + 1; i++) {
         if (i < 0 || i >= gBoard.length) continue;
-        for (var j = board[j] - 1; j <= board[j] + 1; j++) {
+        for (var j = pos.j - 1; j <= pos.j + 1; j++) {
             if (j < 0 || j >= gBoard[i].length) continue;
-            if (i === board[i] && j === board[j]) continue;
-            var currItem = board[i][j];
-            console.log(currItem);
-            if (currItem === MINE) currIte[i].minesAroundCount++;
+            if (i === pos.i && j === pos.j) continue;
+            var currCell = gBoard[i][j];
+            if (currCell.isMine) count++;
         }
     }
-    // console.log(count);
-    // console.log(board);
+    // console.log(gBoard);
     return count;
 }
 
 
+function cellClicked(elCell, pos) {
+    // console.log(ev);
+    console.log(elCell);
+    // if(ev.which === 3){
+    //  console.log("Right mouse button clicked on element with id myId");
+    // }
 
-function cellClicked(elCell, i, j) {
-
+    // window.oncontextmenu = function () {
+    //     if (ev.which === 3)
+    //         elCell.innerHTML = '🚩';
+    // }
 }
+
+    // checkIfGameOver();
+//     // if (!elCell.isShown && elCell.classList.contains('cell')) elCell.style.display = "none";
+// }
 
 // cellMarked(elCell) 
 
-// checkGameOver()
+// checkIfGameOver(){
+//     stop();
+// }
 
 // expandShown(board, elCell, i, j)
+
+// function stopwatch() {
+//     var elTimer = document.querySelector('.timer');
+//     elTimer.innerHTML = gTime;
+//     gTime++
+// }
+
+// function setTime() {
+//     setTimeInterval = setInterval(stopwatch, 1000);
+// }
+
+// function stop() {
+//     clearInterval(setTimeInterval);
+// }
+
+
+// function getLavel(elBtn) {
+//     console.log(elBtn);
+//     var levelSelected = elBtn.innerText;
+//     if (levelSelected === 'Extreme!') {
+//         bordSize = 144;
+//     } else if (levelSelected === 'Hard') {
+//         bordSize = 64;
+//     } else {
+//         bordSize = 16;
+//     }
+//     renderBoard();
+// }
